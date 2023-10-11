9e21217ba9737e6a9922396e471ae178446a7344
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-10-11 13:10:35 +0100

    alx

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,15 +8,15 @@
  * Return: largest number
  */
 
-int largest_number(int a, int b, int c)
+int	largest_number(int a, int b, int c)
 {
 	int largest;
-	
-	if (a > b && a > c)
+
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
@@ -24,6 +24,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	
+
 	return (largest);
 }
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -7,7 +7,7 @@
  *
  * Return: Always 0 (Success)
  */
-void	main(int i)
+void	positive_or_negative(int i)
 {
 	if (i > 0)
 		printf("%d is positive\n", i);
