34f0f72d059ab1bb72f8953d4a768712d9acc4fd
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-10-12 18:57:41 +0100

    alx

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ *main - entry point
+ *
+ *Return:0(success)
+ *
+ *Description:testing above function
+ *
+ */
+int	main(void)
+{
+	long int n, max, i;
+
+	max = -1;
+	n = 612852475143;
+
+	while (n % 2 == 0)
+	{
+		max = 2;
+		n = n / 2;
+	}
+	for (i = 3; i <= n / 2; i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
+	if (n > 2)
+	{
+		max = n;
+	}
+	printf("%ld\n", max);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *print_number - prints an integer
+ *
+ *@n:input
+ *
+ *Return:void
+ *
+ *Description:prints an integer
+ *
+ */
+void	print_number(int n)
+{
+	unsigned int	nbr;
+	
+	nbr = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		nbr *= -1;
+	}
+	if (nbr < 10)
+	{
+		_putchar(nbr + '0');
+	}
+	else
+	{
+		print_number(nbr / 10);
+		print_number(nbr % 10);
+	}
+}
