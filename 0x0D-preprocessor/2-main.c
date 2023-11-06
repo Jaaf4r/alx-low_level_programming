25915d43f65cec87824ff33b1d4b05a11df8b9c6
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-11-06 12:44:27 +0100

    alx

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point, prints file name it was compiled from
+ *
+ * Return: 0 (Success)
+ */
+
 int	main(void)
 {
 	printf("%s\n", __FILE__);
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,1 +1,4 @@
+#ifndef
+#define SUM_H
 #define SUM(x, y) ((x)+(y))
+#endif
