8e26b72ed7c5ec875e2206929f2f3ac376e1d428
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-11-16 19:25:50 +0100

    alx

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,10 +3,11 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - prints a sentence before the main 
+ * first - prints a sentence before the main
  * function is executed
  */
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
