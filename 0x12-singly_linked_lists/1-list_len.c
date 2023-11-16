44b9921a5c2e25af03bbf5836c5507a4ff8e6f15
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-11-16 19:14:45 +0100

    alx

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,7 +3,7 @@
 /**
  * list_len - returns the number of elements in a linked list
  * @h: linked list
- * Return: no. of elements 
+ * Return: no. of elements
  */
 size_t list_len(const list_t *h)
 {
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -23,5 +23,5 @@ list_t *add_node(list_t **head, const char *str)
 	new->next = (*head);
 	*head = new;
 
-	return(*head);
+	return (*head);
 }
