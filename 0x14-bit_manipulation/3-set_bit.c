8ce4fc9737e8c01016a0c1cb35cfc2c8da72f5d2
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-11-23 23:42:01 +0100

    alx

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * set_bit - sets the value of a bit to 1 at a given index
  * @n: parameter
  * @index: index
- * Return: value of a a bit
+ * Return: value
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits you would need to flip 
+ * flip_bits - returns the number of bits you would need to flip
  * to get from one number to another
  * @n: parameter
  * @m: flipped
