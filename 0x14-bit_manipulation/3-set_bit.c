5d5acca4819b8ffd6fae544734ae194f0b238bd5
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-11-23 23:55:35 +0100

    alx

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * set_bit - sets the value of a bit to 1 at a given index
@@ -9,13 +8,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int m;
-
-	if (index > 63)
+	if (index > sizeof(n) * 8)
 		return (-1);
-
-	m = 1 << index;
-	*n = (*n | m);
-
+	*n |= (1 << index);
 	return (1);
 }
