34c93d26280a9def15b613c1420f83f3cb563d26
Jaaf4r <jaafar.bouhnine@gmail.com>
2023-11-23 23:47:20 +0100

    alx

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,8 +9,13 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > sizeof(n) * 8)
+	unsigned int m;
+
+	if (index > 63)
 		return (-1);
-	*n |= (1 << index);
+
+	m = 1 << index;
+	*n = (*n | m);
+
 	return (1);
 }
